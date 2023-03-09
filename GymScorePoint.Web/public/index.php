d3bcdf85a5079d4e2c6dcadaded6be3032d3611d
ahadvirus <ahadvirus@yahoo.com>
2023-03-09 16:56:06 +0330

    Add namespaces to container and file initializer

--- GymScorePoint.Web/public/index.php
+++ GymScorePoint.Web/public/index.php
@@ -2,6 +2,11 @@
 
 require_once '..\app\Packages\Ahada\Helpers\initializer.php';
 
-echo Ahada\Application::dispatch('');
+add_namespace('App\Framework\\', directory_path('app/Packages/Framework'));
+add_namespace('App\\', directory_path('app/'));
+
+App\Bootstrap::execute();
+
+echo Ahada\Application::dispatch('', App\Framework\Collections\Routes::getInstance());
 
 //echo Ahada\Application::dispatch()
